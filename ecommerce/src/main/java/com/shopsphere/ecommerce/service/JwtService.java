9e23508a17484f7b7085132d5150f7df4c17fbb1
hey-pranay <hey.bhatkar@gmail.com>
2026-09-06 14:32:52 +0530

    add the gitignore

--- ecommerce/src/main/java/com/shopsphere/ecommerce/service/JwtService.java
+++ ecommerce/src/main/java/com/shopsphere/ecommerce/service/JwtService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
@@ -15,7 +16,7 @@ public class JwtService {
     private final SecretKey secretKey;
 
     public JwtService(@Value("${jwt.secret}") String secret) {
-        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 
     public String generateToken(User user) {
